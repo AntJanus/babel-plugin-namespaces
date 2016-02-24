91fb6459d33a85697eea1994f6ee803d985c508c
Antonin Januska <antonin@antjanus.com>
2016-02-23 22:21:45 -0700

    Tests

--- test/test.js
+++ test/test.js
@@ -1,2 +1,29 @@
+//testing
 import test from 'ava';
-,f
+
+//babel
+import { transform } from 'babel-core';
+
+import 'babel-register';
+
+test('namespaces', t => {
+  var testCode = `
+    import action from '\actions/some-action/other';
+  `;
+
+  var expected = `
+    import action from './actions/some-action/other';
+  `;
+
+  var result = transform(testCode, {
+    plugins: [
+      ['../src', {
+          actions: './actions'
+        }
+      ]
+    ]
+  });
+
+  console.log(result, 'result');
+  t.is(expected, result);
+});
