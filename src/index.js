79bb4af619d8c9fef4fbc8a34ca445d11163c1ca
Antonin Januska <antonin@antjanus.com>
2016-02-29 22:44:49 -0700

    Path resolution start

--- src/index.js
+++ src/index.js
@@ -2,35 +2,56 @@ import path from 'path';
 
 export default namespacePlugin;
 
-const rootRegex = /^\/(.*?)\//;
-const namespaceRegex = /^\#(.*?)\//i;
+const rootRegex = /^#root\//i;
+const namespaceRegex = /^<(.*?)>\//i;
+
+function handleRoot(source, rawVal, state) {
+  const startPath = process.env.PWD || process.cwd();
+
+  let val = rawVal.replace(rootRegex, '/path/to/root/folder/');
+
+  let current = path.dirname(state.file.opts.filename);
+  let destination = path.join(startPath, val);
+
+  source.value = path.relative(current, destination);
+}
+
+function handleNamespace(source, rawVal, state) {
+  const startPath = process.env.PWD || process.cwd();
+
+  //match to namespace
+  var namespace = namespaceRegex.exec(rawVal);
+
+  if(namespace) {
+    var matchNs = namespace[1];
+    let val = rawVal.replace(namespaceRegex, '');
+
+    let current = path.dirname(state.file.opts.filename);
+    let destination = path.join(startPath, (state.opts.config[matchNs] || ''), val);
+
+    source.value = path.relative(current, destination);
+  }
+}
 
 function namespacePlugin({types: t}) {
   return {
     visitor: {
       ImportDeclaration(path, state) {
-         var source = path.node.source;
-         var rawVal = source.value;
-         var val = '';
+        var source = path.node.source;
+        var rawVal = source.extra.rawValue;
+        var val = '';
 
-         //match to root first
-         var isRoot = rootRegex.exec(rawVal);
+        //match to root first
+        var isRoot = rootRegex.exec(rawVal);
 
-         if(isRoot) {
-           val = rawVal.replace(rootRegex, '/path/to/root/folder/');
-           source.value = val;
-           return;
-         }
+        if(isRoot) {
+          handleRoot(source, rawVal, state);
+          return;
+        }
 
-         //match to namespace
-         var namespace = namespaceRegex.exec(rawVal);
+        handleNamespace(source, rawVal, state);
 
-         if(namespace) {
-           var matchNs = namespace[1];
-           val = rawVal.replace(namespaceRegex, '');
-
-           source.value = (state.opts.config[matchNs] || '') + val;
-         }
+        return;
       }
     }
   };
